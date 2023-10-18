b09025edb27159c70c34e49dd1849ce26302813f
sores <drewmax1234@yahoo.com>
2023-10-18 03:48:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,36 @@ from pygame import mixer
 mixer.init()
 mixer.music.load("audio.mp3")
 
+# Init some variables
+volume_message = "What should the volume be? "
+countdown_time = 10
+volume = 100
 
-def playsound():
+
+# Set variables from user
+def setup():
+    global countdown_time
+    global volume
+
+    countdown_time = int(input("How many seconds would you like to countdown? "))
+    volume = int(input(volume_message))
+
+    # If they try to set volume lower than zero, make them retry
+    if volume < 0:
+        print("The volume cannot be less than zero.")
+        print('\n')
+        volume = int(input(volume_message))
+
+    # If they try to set volume more than 100, make them retry
+    elif volume > 100:
+        print("The volume cannot be greater than 100.")
+        print('\n')
+        volume = int(input(volume_message))
+
+
+def playsound(vol):
     # Set volume to 100 & play the sound
-    mixer.music.set_volume(100)
+    mixer.music.set_volume(vol)
     mixer.music.play()
 
     # Allow time for sound to play & end
@@ -25,9 +51,9 @@ def tick(ti):
 
     # When timer runs out, send a message and play the sound.
     print('Here comes the audio!')
-    playsound()
+    playsound(volume)
 
 
-countdown_time = int(input('How many seconds would you like to countdown? '))
+setup()
 
 tick(countdown_time)
